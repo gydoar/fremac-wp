572d4ce6826889abbaa70ae4f334bdc561ab148f
Andres Vega <gydoar@hotmail.com>
2015-04-20 18:27:58 -0500

    cambios en el contac

--- page-contacto.php
+++ page-contacto.php
@@ -1,3 +1,55 @@
+<?php
+   if(isset($_POST['submitted'])) {
+    if(trim($_POST['contactName']) === '') {
+        $nameError = 'Por favor ingrese su nombre.';
+        $hasError = true;
+    } else {
+        $name = trim($_POST['contactName']);
+    }
+
+    if(trim($_POST['email']) === '')  {
+        $emailError = 'Por favor ingrese su email.';
+        $hasError = true;
+    } else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
+        $emailError = 'Dirreción de email invalida.';
+        $hasError = true;
+    } else {
+        $email = trim($_POST['email']);
+    }
+
+     if(trim($_POST['telefono']) === '') {
+        $telError = 'Por favor ingrese su teléfono.';
+        $hasError = true;
+    } else {
+        $telefono = trim($_POST['telefono']);
+    }
+
+    if(trim($_POST['comments']) === '') {
+        $commentError = 'Por favor ingresa un mensaje.';
+        $hasError = true;
+    } else {
+        if(function_exists('stripslashes')) {
+            $comments = stripslashes(trim($_POST['comments']));
+        } else {
+            $comments = trim($_POST['comments']);
+        }
+    }
+
+    if(!isset($hasError)) {
+        $emailTo = get_option('tz_email');
+        if (!isset($emailTo) || ($emailTo == '') ){
+            $emailTo = get_option('admin_email');
+        }
+        $subject = '[Contacto axemos] From '.$name;
+        $body = "Nombre: $name \n\nEmail: $email \n\nTeléfino: $telefono \n\nComments: $comments";
+        $headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+        wp_mail($emailTo, $subject, $body, $headers);
+        $emailSent = true;
+    }
+
+} ?>
+
 <?php get_header('page'); ?>
 
  <section class="page">
@@ -11,34 +63,67 @@
         </div>
         
         <div class="contacto">
-            <form method="post" action="mail.php" name="contactform" >
-              <fieldset>
-                <div class="c1">
-                  <div class="form-field">
-                    <label for="name">Nombre:*</label>
-                    <span><input type="text" name="name" id="name" required/></span>
-                  </div>
-                  <div class="form-field">
-                    <label for="name">Teléfono:*</label>
-                    <span><input type="text" name="phone" id="phone" required/></span>
-                  </div>
+             <?php if(isset($emailSent) && $emailSent == true) { ?>
+             <div class="alert alert-success">
+                <p>Gracias, su mensaje ha sido bien recibido.</p>
+            </div>
+            <?php } else { ?>
+
+            <?php if(isset($hasError) || isset($captchaError)) { ?>
+            <div class="alert alert-danger alert-dismissable">
+                <a class="close" data-dismiss="alert">×</a>
+                <h4 class="alert-heading">Disculpe, existen algunos errores.</h4>
+                <p class="error">Por favor inténtalo de nuevo!<p>
                 </div>
-               
-                <div class="c2">
-                  <div class="form-field">
-                    <label for="email">Email:*</label>
-                    <span><input type="email" name="email" id="email" required/></span>
-                  </div>
-                  <div class="form-field">
-                    <label for="message">Mensaje:*</label>
-                    <span><textarea name="message" id="message" required></textarea></span>
-                  </div>
-                </div>
-              </fieldset>
-              <div class="form-click">
-                <span><input type="submit" name="submit" value="ENVIAR" id="submit" /></span>
-              </div>
-            </form> 
+                <?php } ?>
+
+                <form action="<?php the_permalink(); ?>" id="contactForm" method="post">
+                    <fieldset>
+                        <div class="form-group">
+
+                            <input class="form-control" type="text" name="contactName" id="contactName" value="" placeholder="Nombre" />
+                            <span class="input-icon fui-check-inverted"></span>
+                            <?php if(isset($nameError)) { ?>
+                            <p><span class="error"><?=$nameError;?></span></p>
+                            <?php } ?>
+                             <br>
+
+                        </div>
+                        <div class="form-group">
+
+                            <input class="form-control" type="text" name="email" id="email" value="" placeholder="Email" />
+                            <span class="input-icon fui-check-inverted"></span>
+                            <?php if(isset($emailError)) { ?>
+                            <p><span class="error"><?=$emailError;?></span></p>
+                            <?php } ?>
+
+                             <br>
+                        </div>
+                        <div class="form-group">
+
+                            <input class="form-control" type="text" name="telefono" id="telefono" value="" placeholder="Teléfono" />
+                            <span class="input-icon fui-check-inverted"></span>
+                            <?php if(isset($telError)) { ?>
+                            <p><span class="error"><?=$telError;?></span></p>
+                            <?php } ?>
+
+                             <br>
+                        </div>
+                        <div class="form-group">
+
+                            <textarea class="form-control" name="comments" id="commentsText" rows="100" cols="10" placeholder="Mensaje"></textarea>
+                            <?php if(isset($commentError)) { ?>
+                            <p><span class="error"><?=$commentError;?></span></p>
+                            <?php } ?>
+                             <br>
+                        </div>
+                        <div class="form-actions">
+                            <button style="float:right;"type="submit" class="btn btn-success button-primary">Enviar</button>
+                        </div>
+                        <input type="hidden" name="submitted" id="submitted" value="true" />
+                    </fieldset>
+                </form>
+                <?php } ?>
         </div>
       
       </section>
